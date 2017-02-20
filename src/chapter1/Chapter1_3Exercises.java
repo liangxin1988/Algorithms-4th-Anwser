9ead45b7b7772f9461ae33da8501c3d2150f73dd
liangxin <liangxin@mapbar.com>
2017-02-20 16:15:08 +0800

    更新1.3 5 6 7 8 9题

--- src/chapter1/Chapter1_3Exercises.java
+++ src/chapter1/Chapter1_3Exercises.java
@@ -1,7 +1,10 @@
 package chapter1;
 
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
 import framework.Title;
 import Answer.BaseChapter;
+import Answer.ChapterUtil;
 import static edu.princeton.cs.algs4.StdOut.*;
 
 /**
@@ -109,7 +112,80 @@ public class Chapter1_3Exercises  extends BaseChapter{
 	
 	@Title("1.3.5")
 	public static void question5(){
-		
+		ChapterUtil.questionLue();
+	}
+	
+	@Title("1.3.6")
+	public static void question6(){
+		println("倒序排列");
+		Stack<String> stack = new Stack<String>();
+		Queue<String> q = new Queue();
+		for(int i = 0;i<9;i++){
+			q.enqueue(i + "");
+		}
+		println("操作前："+q);
+		while(!q.isEmpty()){
+			stack.push(q.dequeue());  //由于栈的后进先出特性，使得栈可以用来进行倒序输出
+		}
+		while(!stack.isEmpty()){
+			q.enqueue(stack.pop());
+		}
+		println("操作后："+q);
+	}
+	
+	@Title("1.3.7")
+	public static void question7(){
+		FixedCapacityStackOfStrings fixedCapacityStackOfStrings = new FixedCapacityStackOfStrings(5);
+		fixedCapacityStackOfStrings.push("1");
+		fixedCapacityStackOfStrings.push("2");
+		println(fixedCapacityStackOfStrings);
+		println("返回最近添加的值："+fixedCapacityStackOfStrings.peek()+",但是不弹出");
+		println(fixedCapacityStackOfStrings);  //打印发现与peek前没有变化
+		println("返回最近添加的值："+fixedCapacityStackOfStrings.pop()+",并弹出");
+		println(fixedCapacityStackOfStrings);  //pop后元素变少了
+	}
+	
+	@Title("1.3.8")
+	public static void question8(){
+		//DoublingStackOfStrings ?
+		ChapterUtil.questionNo();
+	}
+	
+	@Title("1.3.9")
+	public static void question9(String str){  //这个题好难，做出来了但是没有充分测试也没啥把握，大家帮忙多给些用例测试一下吧
+		if(str == null){
+			str = "1+2)*3-4)*5-6)))";  //为了解析方便，所有数字为1位数
+		}
+		Stack<Stack<String>> result = new Stack<>();  //用来保存结果，其中的每一个栈是一个运算因子（如数字3或者已经拼接好的算式（3 + 4））
+		for(char s : str.toCharArray()){
+			switch(s){
+			case ')':  //如果是右括号，要将之前的数据拼装为完整的算式
+				Stack<String> c1 = new Stack<>();
+				c1.push(")");
+				for(int i = 0;i<3;i++){  //取出最后入栈的3个因式（两个因式和一个运算符），入临时栈c1
+					Stack<String> c3 = result.pop();
+					while(!c3.isEmpty()){
+						c1.push(c3.pop());
+					}
+				}
+				c1.push("(");  //加入左括号，拼接成完整的因式
+				Stack<String> c4 = new Stack<>();
+				c4.push(c1.toString());
+				result.push(c4);  //将拼接完整的因式封装为栈推入result栈
+				break;
+			default:  //如果是普通字符，将字符包装为栈存入result栈中
+				Stack<String> c2 = new Stack<>(); 
+				c2.push(s + "");
+				result.push(c2);
+				break;
+			}
+//			println(result);  //打印轨迹
+		}
+		Stack<String> hehe = new Stack<>();  //由于隐式间是倒序排列的，所以需要定义另一个栈，使栈中的元素可以倒序排列
+		while(!result.isEmpty()){
+			hehe.push(result.pop().toString());
+		}
+		println(hehe);
 	}
 	
 	public static class FixedCapacityStackOfStrings{
@@ -133,6 +209,13 @@ public class Chapter1_3Exercises  extends BaseChapter{
 		
 		public boolean isFull(){
 			return N == a.length;  //当N等于a.length时，就无法通过a[N]访问元素了，此时数组已经没有空位
+		}
+		
+		/**
+		 * 返回最近添加的元素，而不弹出
+		 * */
+		public String peek(){
+			return a[N - 1];  //与pop相比，不修改N的值，即可实现不弹出
 		}
 		
 		@Override
