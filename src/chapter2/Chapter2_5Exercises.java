b968afe4f01f8ea25dc5e65125c44ead5f4e2c25
lx <jay_111@163.com>
2017-03-04 22:09:36 +0800

    2.5.6

--- src/chapter2/Chapter2_5Exercises.java
+++ src/chapter2/Chapter2_5Exercises.java
@@ -4,11 +4,14 @@ import Answer.BaseChapter;
 import Answer.ChapterUtil;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Selection;
+import edu.princeton.cs.algs4.StdRandom;
 import framework.Title;
 import static edu.princeton.cs.algs4.StdOut.*;
 
 import java.util.Arrays;
 import java.util.Comparator;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.ParagraphAction;
 
 public class Chapter2_5Exercises extends BaseChapter {
 
@@ -47,6 +50,75 @@ public class Chapter2_5Exercises extends BaseChapter {
 		println();
 	}
 	
+	@Title("2.5.6")
+	public static void question6(){
+		Integer[] is = new Integer[10];
+		for(int i = 0;i < is.length;i++){
+			is[i] = i;
+		}
+
+		for(int i = 0;i<is.length;i++){
+			println(select(is, 0, is.length - 1, i));
+		}
+	}
+	
+	public static Comparable select(Comparable[] a,int start,int end,int k){
+		int mid = partition(a, start, end);
+		if(mid == k){
+			return a[mid];
+		}
+		if(mid > k){
+			return select(a,start,mid - 1,k);
+		}else if(mid < k){
+			return select(a,mid + 1,end,k);
+		}
+		return null;
+	}
+	
+	public static void sort(Comparable[] a,int start,int end){
+		if(start >= end){
+			return;
+		}
+		int mid = partition(a,start,end);
+		println("mid = "+mid);
+		sort(a,start,mid - 1);
+		sort(a,mid + 1,end);
+	}
+	
+	public static int partition(Comparable[] a,int start,int end){
+		int i = start;
+		int j = end + 1;
+		Comparable min = a[start];
+		while(true){
+			while(i < end){
+				if(less(min,a[++i])){
+					break;
+				}
+			}
+			while(j > start){
+				if(less(a[--j],min)){
+					break;
+				}
+			}
+			if(i >= j){
+				break;
+			}
+			exch(a, i, j);
+		}
+		exch(a,start,j);
+		return j;
+	}
+	
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+	
+    private static void exch(Object[] a, int i, int j) {
+        Object swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    
 	public static class IndexNode implements Comparable<IndexNode>{
 		int index;
 		int content;
