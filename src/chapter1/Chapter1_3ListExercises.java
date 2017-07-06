9e42d45a335d1122b6955a8078e2e2f0ebce2949
liangxin <liangxin@mapbar.com>
2017-07-06 12:12:40 +0800

    提交1.3.31

--- src/chapter1/Chapter1_3ListExercises.java
+++ src/chapter1/Chapter1_3ListExercises.java
@@ -1,6 +1,7 @@
 package chapter1;
 
 import edu.princeton.cs.algs4.StdRandom;
+import linked.DoubleLinked;
 import linked.SimpleLinked;
 import queue.CircularLinkedQueue;
 import util.ChapterUtil;
@@ -169,4 +170,53 @@ public class Chapter1_3ListExercises extends BaseChapter {
             println(circularLinkedQueue);  //打印轨迹
         }
     }
+
+    @Title("1.3.30")
+    public static void question30(){
+        ChapterUtil.questionLue();
+    }
+
+    @Title("1.3.31")
+    public static void question31(){
+		DoubleLinked<Integer> doubleLinked = new DoubleLinked<>();
+		String format = "%-9s:";
+		for(int i = 0; i<3; i++){
+			doubleLinked.insertFirst(i);
+			printf(format,"在头部添加节点"+i);println(doubleLinked);
+		}
+
+		for(int i = 3;i<5;i++){
+			doubleLinked.insertLast(i);
+			printf(format,"在尾部添加节点"+i);println(doubleLinked);
+		}
+
+		for(int i = 0;i<3;i++){
+			doubleLinked.deleteFirst();
+			printf(format,"从头部删除节点");println(doubleLinked);
+		}
+
+		while(!doubleLinked.isEmpty()){
+			doubleLinked.deleteLast();
+			printf(format,"从尾部删除节点");println(doubleLinked);
+		}
+
+		for(int i = 0;i<5;i++){
+			doubleLinked.insertLast(i);
+		}
+		printf(format,"重新构造单链表");println(doubleLinked);
+		doubleLinked.insertItemForPre(doubleLinked.findNodeByIndex(0),10);
+		printf(format,"在第一个元素前插入元素");println(doubleLinked);
+		doubleLinked.insertItemForPre(doubleLinked.findNodeByIndex(2),15);
+		printf(format,"在中间位置插入元素");println(doubleLinked);
+
+		doubleLinked.insertItemForNext(doubleLinked.findNodeByIndex(4),20);
+		printf(format,"在中间位置插入元素");println(doubleLinked);
+		doubleLinked.insertItemForNext(doubleLinked.findNodeByIndex(7),25);
+		printf(format,"在最后一个元素后插入元素");println(doubleLinked);
+
+		while(!doubleLinked.isEmpty()){
+			doubleLinked.deleteNode(doubleLinked.findNodeByIndex(0));
+			printf(format,"删除第一个元素");println(doubleLinked);
+		}
+	}
 }
