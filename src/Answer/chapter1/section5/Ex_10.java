7e703c127aff42f19ca80359094b96f9a834c556
liangxin <liangxin@mapbar.com>
2017-07-22 16:17:44 +0800

    提交1.5.9和1.5.10

---
+++ src/Answer/chapter1/section5/Ex_10.java
@@ -0,0 +1,14 @@
+package Answer.chapter1.section5;
+
+import Answer.base.Answer0;
+import util.ChapterUtil;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+public class Ex_10 extends Answer0 {
+    @Override
+    public void answer() {
+        ChapterUtil.questionLue();
+    }
+}
---
+++ src/Answer/chapter1/section5/Ex_9.java
@@ -0,0 +1,15 @@
+package Answer.chapter1.section5;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+@Fixme(info = "暂时不考虑绘图")
+public class Ex_9 extends Answer0 {
+    @Override
+    public void answer() {
+        println("树的高度为4（1->6->5->4->8）,超出了lg10。所以不可能为加权quick-union算法得到的结果");
+    }
+}
