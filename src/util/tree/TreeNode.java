365fd492d55d0a0caf8a4bba09f6623d95032a55
liangxin <liangxin@mapbar.com>
2017-07-24 17:07:43 +0800

    提交一些暂时没用的东西

---
+++ src/util/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package util.tree;
+
+/**
+ * 一个树中的节点
+ */
+public  class  TreeNode<Item> {
+    //节点包含的信息
+    private Item item;
+    /**左子树*/
+    private TreeNode<Item> left;
+    /**右子树*/
+    private TreeNode<Item> right;
+
+}
