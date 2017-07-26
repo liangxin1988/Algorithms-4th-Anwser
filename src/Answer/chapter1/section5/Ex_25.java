07aedaf4e7772082acc187ce29536d6e80f481c2
liangxin <liangxin@mapbar.com>
2017-07-26 13:34:07 +0800

    提交1.5中剩余的题（都未做，只是想进入下一章）

---
+++ src/Answer/chapter1/section5/Ex_25.java
@@ -0,0 +1,12 @@
+package Answer.chapter1.section5;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+@Fixme
+public class Ex_25 extends Answer0 {
+    @Override
+    public void answer() {
+
+    }
+}
