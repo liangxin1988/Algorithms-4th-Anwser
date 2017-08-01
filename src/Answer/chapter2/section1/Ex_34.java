0fb01dcabd89e5e6ac09ffdb2893e7d6a72220b8
liangxin <liangxin@mapbar.com>
2017-08-01 11:55:28 +0800

    提交2.1所有剩余题目（没有答案），为了尽快进入下一章节

---
+++ src/Answer/chapter2/section1/Ex_34.java
@@ -0,0 +1,13 @@
+package Answer.chapter2.section1;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+import util.ChapterUtil;
+
+@Fixme
+public class Ex_34 extends Answer0 {
+    @Override
+    public void answer() {
+        ChapterUtil.questionNo();
+    }
+}
