e5231d451086dea0a70c8ce50dfed98ca64f3e46
liangxin <liangxin@mapbar.com>
2017-08-02 13:39:18 +0800

    提交2.2.1 - 2.2.4

---
+++ src/Answer/chapter2/section2/Ex_1.java
@@ -0,0 +1,16 @@
+package Answer.chapter2.section2;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+import util.ChapterUtil;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+@Fixme(info = "暂时自己画图，等待绘图框架")
+public class Ex_1 extends Answer0 {
+    @Override
+    public void answer() {
+        ChapterUtil.questionNo();
+    }
+}
---
+++ src/Answer/chapter2/section2/Ex_2.java
@@ -0,0 +1,13 @@
+package Answer.chapter2.section2;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+import util.ChapterUtil;
+
+@Fixme(info = "暂时自己画图，等待绘图框架")
+public class Ex_2 extends Answer0 {
+    @Override
+    public void answer() {
+        ChapterUtil.questionNo();
+    }
+}
