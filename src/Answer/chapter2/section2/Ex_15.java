073d8acea3259ce3c5bbfb5b752c45cd4f2462d8
liangxin <liangxin@mapbar.com>
2017-08-04 13:34:13 +0800

    简化2.2.15代码

--- src/Answer/chapter2/section2/Ex_15.java
+++ src/Answer/chapter2/section2/Ex_15.java
@@ -6,12 +6,15 @@ import data_structure.queue.Queue;
 import data_structure.queue.QueueUtil;
 import util.ChapterUtil;
 
+import java.util.Arrays;
+
 import static edu.princeton.cs.algs4.StdOut.println;
 
 public class Ex_15 extends Answer0 {
     @Override
     public void answer() {
         int[] array = ChapterUtil.getRandomIntArray(10);
+        println(Arrays.toString(array));
         Queue<Queue<Integer>> queue = new LinkedQueue();
         for(int i : array){
             Queue<Integer> q = new LinkedQueue<>();
@@ -19,10 +22,7 @@ public class Ex_15 extends Answer0 {
             queue.enqueue(q);
         }
         while(queue.size() != 1){
-            Queue<Integer> dequeue = queue.dequeue();
-            Queue<Integer> dequeue1 = queue.dequeue();
-            Queue<Integer> item = QueueUtil.mergeQueue(dequeue, dequeue1);
-            queue.enqueue(item);
+            queue.enqueue(QueueUtil.mergeQueue(queue.dequeue(),queue.dequeue()));
         }
         println(queue.dequeue());
     }
