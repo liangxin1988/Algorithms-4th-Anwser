9dedfad78a4c84c31629c4ea6b8624d937795a9f
liangxin <liangxin@mapbar.com>
2017-08-09 15:05:29 +0800

    提交2.3.1-2.3.9（未完成）

---
+++ src/Answer/chapter2/section3/Ex_3.java
@@ -0,0 +1,15 @@
+package Answer.chapter2.section3;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+import util.ChapterUtil;
+
+/**
+ */
+@Fixme()
+public class Ex_3 extends Answer0 {
+    @Override
+    public void answer() {
+        ChapterUtil.questionNo();
+    }
+}
---
+++ src/Answer/chapter2/section3/Ex_6.java
@@ -0,0 +1,16 @@
+package Answer.chapter2.section3;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+import util.ChapterUtil;
+
+/**
+ */
+@Fixme
+public class Ex_6 extends Answer0 {
+    @Override
+    public void answer() {
+        ChapterUtil.questionNo();
+    }
+
+}
