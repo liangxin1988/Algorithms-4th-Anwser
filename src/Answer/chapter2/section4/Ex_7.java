d727394adaaaa8045c63baeee9b6740bdbd8b707
liangxin <liangxin@mapbar.com>
2017-08-11 13:25:33 +0800

    提交2.4.7 和 2.4.8

---
+++ src/Answer/chapter2/section4/Ex_7.java
@@ -0,0 +1,20 @@
+package Answer.chapter2.section4;
+
+import Answer.base.Answer0;
+import Answer.base.Answer1String;
+import data_structure.max_pq.HeapMaxPQ;
+
+import java.util.Arrays;
+
+import static edu.princeton.cs.algs4.StdOut.println;
+
+/**
+ */
+public class Ex_7 extends Answer0 {
+    @Override
+    public void answer() {
+        println("第2大元素只可能为位置2 ~ 3");
+        println("第3大元素只可能为位置2 ~ 7");
+        println("第4大元素只可能为位置2 ~ 15");
+    }
+}
---
+++ src/Answer/chapter2/section4/Ex_8.java
@@ -0,0 +1,16 @@
+package Answer.chapter2.section4;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.println;
+
+/**
+ */
+public class Ex_8 extends Answer0 {
+    @Override
+    public void answer() {
+        println("第2小元素只可能为位置8 ~ 31的位置上");
+        println("第3小元素只可能为位置4 ~ 31");
+        println("第4小元素只可能为位置2 ~ 31");
+    }
+}
