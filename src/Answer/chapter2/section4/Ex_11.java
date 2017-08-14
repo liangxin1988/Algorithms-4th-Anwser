6dccb7015f8d8e51b443569398564e627dc05fea
liangxin <liangxin@mapbar.com>
2017-08-14 11:43:41 +0800

    提交2.4.11

---
+++ src/Answer/chapter2/section4/Ex_11.java
@@ -0,0 +1,13 @@
+package Answer.chapter2.section4;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+public class Ex_11 extends Answer0 {
+    @Override
+    public void answer() {
+        println("无序数组的插入时间复杂度为1，而删除的时间复杂度为N。当插入元素次数远远大于删除次数的时候应使用无需数组");
+    }
+}
