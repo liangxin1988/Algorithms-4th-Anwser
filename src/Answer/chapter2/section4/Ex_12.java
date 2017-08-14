f8c7042cd9f01cd806db70f09eac6e68d6abbb28
liangxin <liangxin@mapbar.com>
2017-08-14 11:54:08 +0800

    提交2.4.12

---
+++ src/Answer/chapter2/section4/Ex_12.java
@@ -0,0 +1,13 @@
+package Answer.chapter2.section4;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+public class Ex_12 extends Answer0 {
+    @Override
+    public void answer() {
+        println("有序数组插入时间复杂度为N，删除最大元素时间复杂度为1，如果插入次数远大于查找和删除次数，则应使用有序数组");
+    }
+}
