02ccc20eaf4d434987c48a56f9ead154c92bb4e0
liangxin <liangxin@mapbar.com>
2017-08-14 11:41:17 +0800

    提交2.4.9 和 2.4.10

---
+++ src/Answer/chapter2/section4/Ex_10.java
@@ -0,0 +1,13 @@
+package Answer.chapter2.section4;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+public class Ex_10 extends Answer0 {
+    @Override
+    public void answer() {
+        println("其父节点为(k - 1)/2(向下取整)，子节点为2k + 1和2k + 2");
+    }
+}
---
+++ src/Answer/chapter2/section4/Ex_9.java
@@ -0,0 +1,14 @@
+package Answer.chapter2.section4;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+/**
+ */
+@Fixme
+public class Ex_9 extends Answer0{
+    @Override
+    public void answer() {
+
+    }
+}
