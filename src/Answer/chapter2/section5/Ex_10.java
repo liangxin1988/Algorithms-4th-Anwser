0a1637c78d4d0549b4832131cc23acf40a816bed
liangxin <liangxin@mapbar.com>
2017-08-15 12:34:41 +0800

    提交2.5.10

---
+++ src/Answer/chapter2/section5/Ex_10.java
@@ -0,0 +1,46 @@
+package Answer.chapter2.section5;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+public class Ex_10 extends Answer0 {
+    @Override
+    public void answer() {
+        Version v1 = new Version("115.1.1");
+        Version v2 = new Version("115.10.1");
+		println(less(v1,v2));
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private class Version implements Comparable<Version>{
+        int first;
+        int secend;
+        int third;
+
+        public Version(String version){
+            String[] str = version.split("\\.");
+            first = Integer.parseInt(str[0]);
+            secend = Integer.parseInt(str[1]);
+            third = Integer.parseInt(str[2]);
+        }
+
+        @Override
+        public int compareTo(Version o) {
+            if(this.first != o.first){
+                return this.first - o.first;
+            }
+            if(this.secend != o.secend){
+                return this.secend - o.secend;
+            }
+
+            return this.third - o.third;
+        }
+
+
+    }
+}
