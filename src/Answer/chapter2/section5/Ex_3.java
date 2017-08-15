363770c84780136ed2f465baaf6fa75fd91ddd5b
liangxin <liangxin@mapbar.com>
2017-08-15 12:01:20 +0800

    提交2.5.3

---
+++ src/Answer/chapter2/section5/Ex_3.java
@@ -0,0 +1,40 @@
+package Answer.chapter2.section5;
+
+import Answer.base.Answer0;
+import chapter2.Chapter2_5Exercises;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+public class Ex_3 extends Answer0 {
+    @Override
+    public void answer() {
+        Balance a = new Balance();
+        Balance b = new Balance();
+        Balance c = new Balance();
+        a.amount = 0;
+        b.amount = a.amount + 0.005;
+        c.amount = b.amount + 0.005;
+        println("b比a："+b.compareTo(a));
+        println("c比b："+c.compareTo(b));
+        println("c比a："+c.compareTo(a));
+        println("不满足传递性，a与b相等，b与c相等，但是a与c不相等");
+        println("修改方法：去掉加减0.005的代码");
+    }
+
+    public static class Balance implements Comparable<Balance>{
+
+        private double amount;
+        @Override
+        public int compareTo(Balance that) {
+            if(this.amount < that.amount - 0.005){
+                return -1;
+            }
+            if(this.amount > that.amount + 0.005){
+                return 1;
+            }
+            return 0;
+        }
+
+    }
+}
--- src/chapter2/Chapter2_5Exercises.java
+++ src/chapter2/Chapter2_5Exercises.java
@@ -16,17 +16,7 @@ import util.ChapterUtil;
 public class Chapter2_5Exercises {
 
 	public static void question3(){
-		Balance a = new Balance();
-		Balance b = new Balance();
-		Balance c = new Balance();
-		a.amount = 0;
-		b.amount = a.amount + 0.005;
-		c.amount = b.amount + 0.005;
-		println("b比a："+b.compareTo(a));
-		println("c比b："+c.compareTo(b));
-		println("c比a："+c.compareTo(a));
-		println("不满足传递性，a与b相等，b与c相等，但是a与c不相等");
-		println("修改方法：去掉加减0.005的代码");
+
 	}
 	
 	public static void question5(){
@@ -214,19 +204,5 @@ public class Chapter2_5Exercises {
 	}
 
 	
-	public static class Balance implements Comparable<Balance>{
 
-		private double amount;
-		@Override
-		public int compareTo(Balance that) {
-			if(this.amount < that.amount - 0.005){
-				return -1;
-			}
-			if(this.amount > that.amount + 0.005){
-				return 1;
-			}
-			return 0;
-		}
-		
-	}
 }
