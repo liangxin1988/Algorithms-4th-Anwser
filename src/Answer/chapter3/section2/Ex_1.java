aae4800c00083e6ceacf2b0b3b7d32ed94ad43d1
liangxin <liangxin@mapbar.com>
2017-08-22 11:51:53 +0800

    提交3.2.1(未完成)

---
+++ src/Answer/chapter3/section2/Ex_1.java
@@ -0,0 +1,13 @@
+package Answer.chapter3.section2;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+/**
+ */
+@Fixme(info = "涉及绘图先不做")
+public class Ex_1 extends Answer0{
+    @Override
+    public void answer() {
+    }
+}
