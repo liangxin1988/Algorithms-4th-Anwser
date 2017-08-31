a00f06faabcef50d283c7855a33bb54a2bad10fb
liangxin <liangxin@mapbar.com>
2017-09-01 06:26:55 +0800

    提交4.1.12

---
+++ src/Answer/chapter4/section1/Ex_12.java
@@ -0,0 +1,13 @@
+package Answer.chapter4.section1;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.print;
+import static edu.princeton.cs.algs4.StdOut.println;
+
+public class Ex_12 extends Answer0 {
+    @Override
+    public void answer() {
+        println("如果v和w在同一条路径上（根节点到v的最短路径经过w，或者根节点到w的最短路径经过v）就可以通过两者与根节点路径的差的绝对值计算。否则不行");
+    }
+}
