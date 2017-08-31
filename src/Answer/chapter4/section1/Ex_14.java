338d3d134817f9b060d5ac50e4836b705f011546
liangxin <liangxin@mapbar.com>
2017-09-01 06:46:53 +0800

    提交4.1.14

---
+++ src/Answer/chapter4/section1/Ex_14.java
@@ -0,0 +1,14 @@
+package Answer.chapter4.section1;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.println;
+
+/**
+ */
+public class Ex_14 extends Answer0 {
+    @Override
+    public void answer() {
+        println("用栈代替队列，相当于使用了深度优先，无法得到最短路径");
+    }
+}
