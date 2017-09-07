1bd75bd9988a2c63cc1edbfb3c6bb64085529b97
liangxin <liangxin@mapbar.com>
2017-09-07 13:52:53 +0800

    提交4.2.8(未完成)

---
+++ src/Answer/chapter4/section2/Ex_8.java
@@ -0,0 +1,12 @@
+package Answer.chapter4.section2;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+@Fixme(info = "绘图相关暂时忽略")
+public class Ex_8 extends Answer0 {
+    @Override
+    public void answer() {
+
+    }
+}
