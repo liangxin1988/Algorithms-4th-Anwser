22669e5ad28aa592e2b45c03c532e80357c975f2
liangxin <liangxin@mapbar.com>
2017-09-07 13:21:22 +0800

    提交4.2.6（未完成）

---
+++ src/Answer/chapter4/section2/Ex_6.java
@@ -0,0 +1,14 @@
+package Answer.chapter4.section2;
+
+import Answer.base.Answer0;
+import framework.Fixme;
+
+/**
+ */
+@Fixme(info = "编写什么的测试用例")
+public class Ex_6 extends Answer0{
+    @Override
+    public void answer() {
+
+    }
+}
