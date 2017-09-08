d886cddc505eb50be9760e2a3a0e85bdcc3396de
liangxin <liangxin@mapbar.com>
2017-09-08 12:23:00 +0800

    提交4.2.14

---
+++ src/Answer/chapter4/section2/Ex_14.java
@@ -0,0 +1,14 @@
+package Answer.chapter4.section2;
+
+import Answer.base.Answer0;
+
+import static edu.princeton.cs.algs4.StdOut.println;
+
+/**
+ */
+public class Ex_14 extends Answer0{
+    @Override
+    public void answer() {
+        println("原图中v -> w，在其反向图中变为w -> v。原图中的v <-> w，在其反向图中仍然是v <-> w。所有边的强连通性不变。");
+    }
+}
